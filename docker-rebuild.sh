5e72fc1c59ce29df7a18722584564a1d1df5619f
Artem Filippow <a.filippov@qtech.ru>
2026-01-22 12:08:07 +0900

    rebuild script

--- docker-rebuild.sh
+++ docker-rebuild.sh
@@ -3,7 +3,7 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR" || exit
 GIT=$(git pull 2>&1 | head -n 1)
 
-if [[ $GIT =~ Already up to date. ]]; then
+if [[ $GIT =~ Already ]]; then
   echo "$GIT"
   date > "$BASEDIR/updated.txt"
 fi
