9a0c1bcd93f7eebc78fa5e9b66038b6050eb1135
Artem Filippow <a.filippov@qtech.ru>
2026-01-29 18:43:49 +0900

    refactoring to template paradigm

--- abrikosio/settings.py
+++ abrikosio/settings.py
@@ -99,14 +99,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            "loaders": [
-                (
-                    "pypugjs.ext.django.Loader",
-                    ("django.template.loaders.filesystem.Loader", "django.template.loaders.app_directories.Loader",),
-                )
-            ],
-            "builtins": ["pypugjs.ext.django.templatetags", ],
-
         },
     },
 ]
