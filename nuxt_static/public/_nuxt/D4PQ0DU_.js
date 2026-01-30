cee7fab03a8b48018bf3a764a00518a2717d4234
Artem Filippow <a.filippov@qtech.ru>
2026-01-30 09:41:52 +0900

    avatar fix

--- nuxt_static/public/_nuxt/D4PQ0DU_.js
+++
@@ -1,1 +0,0 @@
-import{d as t,Q as e,n}from"./CD7RvFUp.js";const _=t({__name:"logout",setup(s){const{logout:o}=e();return o(),n("/users/login"),(a,r)=>null}});export{_ as default};
---
+++ nuxt_static/public/_nuxt/EhyzITpe.js
@@ -0,0 +1,1 @@
+import{d as t,Q as e,n}from"./ChYXO2yg.js";const _=t({__name:"logout",setup(s){const{logout:o}=e();return o(),n("/users/login"),(a,r)=>null}});export{_ as default};
