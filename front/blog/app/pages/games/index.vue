2afab710a8aa7d1a2f0e42ea59b1dbd023ab55df
Artem Filippow <a.filippov@qtech.ru>
2026-02-05 23:11:11 +0900

    place ships

--- front/blog/app/pages/games/index.vue
+++ front/blog/app/pages/games/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
+import SeaBattle from "~/components/games/seabattle/SeaBattle.vue";
 </script>
 
 <template lang="pug">
 div Games
+  SeaBattle
 </template>
 
 <style scoped>
