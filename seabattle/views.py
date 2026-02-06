1b2c3635307cfe50524f2c2244948550522c0cc6
Artem Filippow <a.filippov@qtech.ru>
2026-02-07 00:22:25 +0900

    fix collision bug

--- seabattle/views.py
+++ seabattle/views.py
@@ -2,7 +2,6 @@ import copy
 import math
 import random
 
-from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
@@ -24,7 +23,7 @@ def check_all_ships(field: list[dict]):
         errors.append('Wrong field data')
     else:
         for ship_size in range(1, 6):
-            ship = list(filter(lambda x: x['ship'] == ship_size, field))
+            ship = list(filter(lambda x: x['ship'] == ship_size and 'isShip' in x, field))
             if not len(ship):
                 errors.append(f'Ship {ship_size} must be placed')
             elif len(ship) != ship_size:
@@ -39,7 +38,7 @@ def has_collision(field, ship, ship_size):
         for row in range(cell['row'] - 1, cell['row'] + 2):
             for col in range(cell['col'] - 1, cell['col'] + 2):
                 idx = next((index for index, d in enumerate(field) if d['row'] == row and d['col'] == col), None)
-                if idx and field[idx]['ship'] != ship_size:
+                if idx and field[idx]['ship'] != ship_size and 'isShip' in field[idx]:
                     return True
     return False
 
@@ -96,44 +95,53 @@ def place_random_ships(sb):
     def check_collision(c):
         if horizontal:
             for col in range(c['col'], c['col'] + ship_size + 1):
-                f = found_in_field(field_cut_borders, {'row': c['row'], 'col': col})
+                f = found_in_field(field, {'row': c['row'], 'col': col})
                 if f and 'ship' in f:
                     return False
         else:
             for row in range(c['row'], c['row'] + ship_size + 1):
-                f = found_in_field(field_cut_borders, {'row': row, 'col': c['col']})
+                f = found_in_field(field, {'row': row, 'col': c['col']})
                 if f and 'ship' in f:
                     return False
         return True
 
     is_horizontal = [True, False]
     field = fill_field(sb)
-    ships = []
-    for ship_size in range(1, 6):
+    ships_field = []
+    for ship_size in range(5, 0, -1):
         horizontal = random.choice(is_horizontal)
-        field_no_ship = list(filter(lambda x: 'ship' not in x, field))
-        field_cut_borders = list(filter(lambda x: x['col'] + ship_size <= sb.cols if horizontal else x['row'] + ship_size <= sb.rows, field))
-        field_no_collision = list(filter(check_collision, field_cut_borders))
 
-        field_free = field_no_collision
+
+        field_no_collision = list(filter(check_collision, field))
+        field_no_ship = list(filter(lambda x: 'ship' not in x, field_no_collision))
+        field_cut_borders = list(filter(lambda x: x['col'] + ship_size <= sb.cols if horizontal else x['row'] + ship_size <= sb.rows, field_no_ship))
+
+        field_free = field_cut_borders
         cell = random.choice(field_free)
         cell['ship'] = ship_size
         cell['isShip'] = True
         cell['horizontal'] = horizontal
         ship = get_ship_cells(cell, sb)
         borders = make_borders(field, ship)
-        ships += ship + borders
+        ships_field += ship + borders
 
-    return ships
+    return ships_field
+
+def mask_opponent_field(field):
+    return list(filter(lambda x: 'strike' in x, field))
 
 
 class SBApiViewSet(viewsets.GenericViewSet):
     queryset = SeaBattle.objects.all()
     serializer_class = SeaBattleSerializerList
 
-    # @action(detail=False, methods=['GET'])
     def list(self, request):
         return Response(SeaBattleSerializerList(self.queryset, many=True).data)
+
+    def retrieve(self, request, pk=None):
+        sb = SeaBattle.objects.get(pk=pk)
+        # sb.field_op = mask_opponent_field(sb.field_op)
+        return Response(SeaBattleSerializerPlay(sb).data)
 
     @action(detail=True, methods=['GET'], permission_classes=[IsAuthenticated])
     def random(self, request, pk=None):
@@ -141,23 +149,25 @@ class SBApiViewSet(viewsets.GenericViewSet):
         sb.field_my = place_random_ships(sb)
         return Response(SeaBattleSerializerPlay(sb).data)
 
-    @action(detail=True, methods=['GET'])
-    def play(self, request, pk=None):
-        sb = SeaBattle.objects.get(pk=pk, user=request.user)
-        return Response(SeaBattleSerializerPlay(sb).data)
-
     @action(detail=False, methods=['GET'])
-    def start(self, request):
+    def new(self, request):
         sb = SeaBattle(user=request.user)
         sb.save()
         return Response(SeaBattleSerializerCreate(sb).data)
 
-    @action(detail=False, methods=['POST'])
-    def check_field(self, request):
+    @action(detail=True, methods=['POST'])
+    def start(self, request, pk=None):
         field = request.data
         errors = check_all_ships(field)
         if len(errors):
-            return Response(errors, status=status.HTTP_406_NOT_ACCEPTABLE)
-        sb = SeaBattle(user=request.user, field_my=field, field_op=field)
+            return Response({'my': errors, 'op': []}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        sb = self.queryset.get(pk=pk)
+        if sb.is_active:
+            return Response({'my': ['Game already started']}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        sb.field_my = field
+        sb.field_op = place_random_ships(sb)
+        errors = check_all_ships(sb.field_op)
+        if len(errors):
+            return Response({'my': [], 'op': errors}, status=status.HTTP_406_NOT_ACCEPTABLE)
         sb.save()
-        return Response(SeaBattleSerializerCreate(sb).data)
+        return Response(SeaBattleSerializerPlay(sb).data)
